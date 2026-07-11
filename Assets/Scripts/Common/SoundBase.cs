6a3a283eef105a5ec942415984a52bef3d52d129
Keshbel <85021488+Keshbel@users.noreply.github.com>
2026-07-11 18:29:01 +0700

    refactor(audio): replace scene-wide button discovery

--- Assets/Scripts/Common/SoundBase.cs
+++ Assets/Scripts/Common/SoundBase.cs
@@ -1,35 +1,68 @@
-using System;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundBase : MonoBehaviour
 {
-    public AudioSource audioSource;
-    
-    public AudioClip clickSound;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip clickSound;
+    [SerializeField] private Button[] clickButtons;
 
     private void Awake()
     {
-        if (!audioSource)
-            audioSource = GetComponent<AudioSource>();
-        
+        if (audioSource == null)
+        {
+            Debug.LogError("SoundBase requires an explicit AudioSource reference.", this);
+            enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
         SubscribeClickSound();
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeClickSound();
     }
 
     private void SoundClick()
     {
-        audioSource.PlayOneShot(clickSound);
-    }
-    
-    private void SubscribeClickSound()
-    {
-        var buttonList = FindObjectsOfType<Button>().ToList();
-        foreach (var button in buttonList)
+        if (audioSource != null && clickSound != null)
         {
-            button.onClick.AddListener(SoundClick);
+            audioSource.PlayOneShot(clickSound);
         }
     }
 
-    
+    private void SubscribeClickSound()
+    {
+        if (clickButtons == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < clickButtons.Length; i++)
+        {
+            if (clickButtons[i] != null)
+            {
+                clickButtons[i].onClick.AddListener(SoundClick);
+            }
+        }
+    }
+
+    private void UnsubscribeClickSound()
+    {
+        if (clickButtons == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < clickButtons.Length; i++)
+        {
+            if (clickButtons[i] != null)
+            {
+                clickButtons[i].onClick.RemoveListener(SoundClick);
+            }
+        }
+    }
 }
